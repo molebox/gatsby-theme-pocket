73a747a0dfa6ed49ecd9c64896885d2cd97cfd16
Richard Haines <richard.haines@88.131.110>
2019-11-04 16:16:34 +0100

    need to fix env variables in setup package

--- packages/gatsby-theme-pocket-setup/bin/setup.js
+++ packages/gatsby-theme-pocket-setup/bin/setup.js
@@ -6,5 +6,4 @@ let activeEnv = process.env.NODE_ENV || "development" || "production"
 require("dotenv").config({
   path: `.env.${activeEnv}`,
 })
-
-shell.exec(`node node_modules/node-getpocket/authorise --consumerkey ${process.env.POCKET_CONSUMER_KEY}`)
+shell.exec(`node node_modules/node-getpocket/authorise --consumerkey 88326-cf4f8f5d0591e3620ece0459`)
